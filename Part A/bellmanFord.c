f0ec81cf0669e155cd163e5c22d10716dde89202
R Anurag Pillai <46356136+CodesOfAnurag@users.noreply.github.com>
2019-12-24 15:41:41 +0530

    Update bellmanFord.c
    Small Changes

--- Part A/bellmanFord.c
+++ Part A/bellmanFord.c
@@ -1,68 +1,88 @@
-#include <stdio.h>
-#include <stdlib.h>
-int S;
-int parent(int v,int p[20]){
-    if(p[v]==S)
-        return S;
-    printf(" %d <-",p[v]);
-    return parent(p[v],p);
-}
-int bf(int G[20][20] , int V, int E, int edge[20][2]){
-    int i,j,u,v,k,distance[20],p[20],flag=1;
-    for(i=0;i<V;i++){
-        distance[i] = 1000 ;
-        p[i] = -1 ;
-    }
-    printf("Enter source: ");
-    scanf("%d",&S);
-    distance[S]=0 ;
-    for(i=0;i<V-1;i++){
-        for(k=0;k<E;k++){
-            u = edge[k][0];
-            v = edge[k][1];
-            if(distance[u]+G[u][v] < distance[v]){
-                distance[v] = distance[u] + G[u][v] ;
-                p[v]=u;
-            }
-        }
-    }
-    for(k=0;k<E;k++){
-        u = edge[k][0] ;
-        v = edge[k][1] ;
-        if(distance[u]+G[u][v] < distance[v])
-            flag = 0 ;
-    }
-    if(flag){
-        printf("\nFrom source vertex %d to:\n",S);
-        for(i=0;i<V;i++){
-            if(i!=S){
-                printf("Vertex %d -> cost = %d\n",i,distance[i]);
-                printf("path: %d <-",i);
-                printf(" %d\n",parent(i,p));
-            }
-        }
-    }
-    return flag;
+#include<stdio.h>
+#include<stdlib.h>
+
+int src;
+
+int parent(int v, int p[20])
+{
+	if (p[v]==src)
+		return src;
+	printf("%d <- ", p[v]);
+	return parent(p[v], p);
 }
 
-int main(){
-    int V,edge[20][2],G[20][20],i,j,k=0;
-    printf("BELLMAN FORD\n");
-    printf("Enter no. of vertices: ");
-    scanf("%d",&V);
-    printf("Enter graph in matrix form:\n");
-    for(i=0;i<V;i++)
-        for(j=0;j<V;j++){
-            scanf("%d",&G[i][j]);
-            if(G[i][j]!=0){
-                edge[k][0]=i;
-                edge[k++][1]=j;
-            }
-        }
+int bf(int g[20][20], int e[20][2], int V, int E)
+{
+	int dist[20], i, k, u, v, p[20], flag=1;
+	for (i=0; i<V; i++)
+	{
+		dist[i]=1000;
+		p[i]=-1;
+	}
 
-    if(bf(G,V,k,edge))
-        printf("\nNo negative weight cycle\n");
-    else 
-        printf("\nNegative weight cycle exists\n");
-    return 0;
+	dist[src]=0;
+
+	for (i=0; i<V-1; i++)
+		for (k=0; k<E; k++)
+		{
+			u = e[k][0];
+			v = e[k][1];
+			if (dist[v] > dist[u]+g[u][v])
+			{
+				dist[v]=dist[u]+g[u][v];
+				p[v]=u;
+			}
+		}
+
+	for (k=0; k<E; k++)
+	{
+		u = e[k][0];
+		v = e[k][1];
+		if (dist[v] > dist[u]+g[u][v])
+			flag=0;
+	}
+
+	if (flag)
+	{
+		printf("\nFrom Source : %d\n", src);
+		for(i=0; i<V; i++)
+			if (i!=src)
+			{
+				printf("\nNode: %d\n", i);
+				printf("Cost: %d\n", dist[i]);
+				printf("Path: %d <--",i);
+				printf("%d\n\n", parent(i,p));
+				
+			}
+	}
+
+	return flag;
+}
+
+void main()
+{
+	int g[20][20], V, E=0, e[20][2], i, j;
+
+	printf("n(Vertices): ");
+	scanf("%d", &V);
+
+	printf("Source: ");
+	scanf("%d", &src);
+
+	printf("Enter Cost Matrix:\n");
+	for (i=0; i<V; i++)
+		for (j=0; j<V; j++)
+			{
+				scanf("%d", &g[i][j]);
+				if (g[i][j]!=0)
+				{
+					e[E][0]=i;
+					e[E++][1]=j;
+				}
+			}
+
+	if (bf(g,e,V,E))
+		printf("No Negative Cycles\n");
+	else
+		printf("Negative Cycle Exists\n");
 }
