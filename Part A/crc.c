fea0e04eff9ecccaa0106cf2ce36afe4840d033c
R Anurag Pillai <46356136+CodesOfAnurag@users.noreply.github.com>
2019-12-24 15:42:48 +0530

    Update crc.c
    Changes made

--- Part A/crc.c
+++ Part A/crc.c
@@ -1,57 +1,59 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
-#define N strlen(g)
- 
-char t[28],cs[28],g[]="10001000000100001";
-int a,e,c;
- 
+#define N strlen(poly)
+// poly --- can be changed
+char poly[]="10001000000100001", data[30], syn[30];
+int a, e, c;
+
 void xor(){
-    for(c = 1;c < N; c++)
-        cs[c] = (( cs[c] == g[c])?'0':'1');
+    for (c=1; c<N; c++)
+        syn[c] = (syn[c]==poly[c])? '0' : '1';
 }
- 
-void crc(){
-    for(e=0;e<N;e++)
-        cs[e]=t[e];
+
+void crc()
+{
+    for (e=0; e<N; e++)
+        syn[e] = data[e];
+
     do{
-        if(cs[0]=='1')
+        if (syn[0]=='1')
             xor();
-        for(c=0;c<N-1;c++)
-            cs[c]=cs[c+1];
-        cs[c]=t[e++];
+
+        for (c=0; c<N-1; c++)
+            syn[c]=syn[c+1];
+        syn[c] = data[e++];
+
     }while(e<=a+N-1);
 }
- 
-int main()
+
+void main()
 {
-    printf("\nEnter data : ");
-    scanf("%s",t);
-    printf("\nGeneratng polynomial : %s",g);
-    a=strlen(t);
-    for(e=a;e<a+N-1;e++)
-        t[e]='0';
-    printf("\nModified data is : %s",t);
+    printf("Polynomial: %s\n", poly);
+    printf("Data: ");
+    scanf("%s", data);
+    a=strlen(data);
+    // --- padding ---
+    for(e=a; e<a+N-1; e++)
+        data[e]='0';
+    printf("Data before Division: %s\n", data);
+
     crc();
-    printf("\nChecksum is : %s",cs);
-    for(e=a;e<a+N-1;e++)
-        t[e]=cs[e-a];
-    printf("\nFinal codeword is : %s",t);
-    printf("\nTest error detection 0(yes) 1(no)? : ");
-    scanf("%d",&e);
-    if(e==0)
-    {
-        do{
-            printf("\nEnter the position where error is to be inserted : ");
-            scanf("%d",&e);
-        }while(e==0 || e>a+N-1);
-        t[e-1]=(t[e-1]=='0')?'1':'0';
-        printf("\nErroneous data : %s\n",t);
-    }
+    printf("Syndrome Bits: %s\n", syn);
+    // --- replacing padding with syndrome bits ---
+    for(e=a; e<a+N-1; e++)
+        data[e]=syn[e-a];
+    printf("Codeword: %s\n", data);
+
+    printf("--- Testing Error Detection ---\n");
+    e = rand()%a;
+    data[e] = (data[e]=='1')? '0' : '1' ;
+    printf("Error at index %d: %s\n", e, data);
     crc();
-    for(e=0;(e<N-1) && (cs[e]!='1');e++);
-    if(e<N-1)
-        printf("\nError detected\n\n");
+    printf("Syndrome Bits Produced:%s\n", syn);
+    for(e=0; e<N-1 && syn[e]!='1'; e++);
+    if (e<N-1)
+        printf("Error Found\n");
     else
-        printf("\nNo error detected\n\n");
-    return 0;
+        printf("Error Not Found\n");
 }
